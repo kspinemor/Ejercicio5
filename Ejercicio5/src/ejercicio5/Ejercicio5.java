3e4a7a7b615f2bbfff253f2bef7b3a4525eebe12
Kevin Stiven Pineda Morales <kevinpinedamyg@gmail.com>
2016-01-30 08:21:40 +0100

    Merge commit 'eff3de85ec1136ee3485682498c965747a5db43d' into Gnome
    Conflicts:
    	src/ejercicio/pkg5/Ejercicio5.java

--- Ejercicio5/src/ejercicio5/Ejercicio5.java
+++ Ejercicio5/src/ejercicio5/Ejercicio5.java
@@ -15,7 +15,7 @@ public class Ejercicio5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Linux");
+        System.out.println("Linux V1 Drivers");
     }
     
 }
